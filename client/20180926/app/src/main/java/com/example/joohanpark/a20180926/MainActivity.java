90f1d0e08333e27535827eb22ed9bfbce8971eb1
Joohan Park <pajh0509@gmail.com>
2020-03-03 01:04:39 +0900

    Fix bugs

--- client/20180926/app/src/main/java/com/example/joohanpark/a20180926/MainActivity.java
+++ client/20180926/app/src/main/java/com/example/joohanpark/a20180926/MainActivity.java
@@ -8,6 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("hello");
+
+        System.out.println("hello");
+
         setContentView(R.layout.activity_main);
     }
 }
